99ca3444556738402111048c7051cfb9a4dba415
Christopher <chris.brower.96@gmail.com>
2023-02-16 15:35:09 -0700

    final update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let newMerch;
 let meatBanner;
 let cartArray = [];
 let currentMerch;
-let allCheese;  
+let allCheese;
 document.addEventListener("DOMContentLoaded", () => {
   fetch("http://localhost:3000/meats")
     .then((res) => res.json())
@@ -16,12 +16,12 @@ document.addEventListener("DOMContentLoaded", () => {
       });
       //favorite();
     });
-    fetch("http://localhost:3000/cheese")
+  fetch("http://localhost:3000/cheese")
     .then((res) => res.json())
     .then((cheeseItems) => {
       allCheese = cheeseItems;
     });
-    cartListMaker()
+  cartListMaker();
 });
 function renderPage() {
   addBanner("sources/1_098f32f2-739b-4e04-9396-73a5b456cbb9_1600x.jpeg");
@@ -30,6 +30,7 @@ function renderPage() {
     "src",
     "sources/sausage-depot-high-resolution-color-logo (2).png"
   );
+
   logoImg.setAttribute("class", "logo");
   let meats = document.createElement("h2");
   meats.textContent = "Meats";
@@ -46,10 +47,10 @@ function renderPage() {
   addHoverEffect(favs);
   let board = document.createElement("h2");
   board.innerHTML = `Charcuterie board <br> Generator`;
-addHoverEffect(board);
-board.addEventListener("click", () => {
-  showBoard()
-});
+  addHoverEffect(board);
+  board.addEventListener("click", () => {
+    showBoard();
+  });
   let merch = document.createElement("h2");
   merch.textContent = "Merch";
   merch.addEventListener("click", () => {
@@ -81,15 +82,15 @@ function showFavorites() {
     }
   });
 }
-function showBoard(){
+function showBoard() {
   clearPage();
   //addBanner("sources/Test-Banner-2-cropped.jpg");
-  for(let i = 0; i < 3; i++) {
-    let randomIndexCheese = Math.round(Math.random() * (allCheese.length-1));
-    let randomIndexMeat = Math.round(Math.random() * (allMeats.length-1));
-    console.log(randomIndexCheese); 
+  for (let i = 0; i < 3; i++) {
+    let randomIndexCheese = Math.round(Math.random() * (allCheese.length - 1));
+    let randomIndexMeat = Math.round(Math.random() * (allMeats.length - 1));
+    console.log(randomIndexCheese);
     console.log(randomIndexMeat);
-    console.log(allCheese[randomIndexCheese]); 
+    console.log(allCheese[randomIndexCheese]);
     console.log(allMeats[randomIndexMeat]);
     renderCard(allCheese[randomIndexCheese]);
     renderCard(allMeats[randomIndexMeat]);
@@ -97,7 +98,7 @@ function showBoard(){
 }
 function showMerch() {
   clearPage();
-  
+
   // addBanner("sources/Merch+banner-1.png");
   fetch("http://localhost:3000/merch")
     .then((res) => res.json())
@@ -122,7 +123,7 @@ function showCheese() {
 }
 function showMeats() {
   clearPage();
-  
+
   // addBanner("sources/1_098f32f2-739b-4e04-9396-73a5b456cbb9_1600x.jpeg");
   allMeats.forEach((meat) => {
     renderCard(meat);
@@ -145,7 +146,8 @@ function addToCartForm(currentItem) {
   addToCartForm.addEventListener("submit", (e) => {
     e.preventDefault();
     let quantity = parseInt(e.target["quantity-input"].value);
-    if (quantity <= 0) {
+    console.log(quantity);
+    if (quantity < 1 || Number.isNaN(quantity)) {
       alert("Quantity must be a positive number.");
       return;
     }
@@ -156,8 +158,8 @@ function addToCartForm(currentItem) {
       amount: quantity,
     };
     if (cartArray.some((item) => item.name === cartItem.name)) {
-      let index= cartArray.indexof(item.name)
-      cartArrray[index] += quantity
+      let index = cartArray.indexof(item.name);
+      cartArrray[index] += quantity;
       return;
     }
     cartArray.push(cartItem);
@@ -169,58 +171,64 @@ function addToCartForm(currentItem) {
 function cartListMaker() {
   let total = 0;
 
-  if(cartArray.length > 0) {
-  document.querySelector("#cart-container").innerHTML = "";
-  let cartList = document.createElement("ul");
-  cartList.setAttribute("class", "cart-list");
-  cartArray.forEach((item, index) => {
-    let line = document.createElement("li");
-    line.setAttribute("class", "cart-line");
-    let img = document.createElement("img");
-    img.setAttribute("class", "listImg");
-    img.src = item.img;
-    let itemPrice = document.createElement("p");
-    itemPrice.textContent = `$${item.price}/Lbs`;
-    itemPrice.setAttribute("class", "cart-line-item");
+  if (cartArray.length > 0) {
+    document.querySelector("#cart-container").innerHTML = "";
+    let cartList = document.createElement("ul");
+    cartList.setAttribute("class", "cart-list");
+    cartArray.forEach((item, index) => {
+      let line = document.createElement("li");
+      line.setAttribute("class", "cart-line");
+      let img = document.createElement("img");
+      img.setAttribute("class", "listImg");
+      img.src = item.img;
+      let itemPrice = document.createElement("p");
+      itemPrice.textContent = `$${item.price}/Lbs`;
+      itemPrice.setAttribute("class", "cart-line-item");
 
-    let name = document.createElement("p");
-    name.textContent = item.name;
-    name.setAttribute("class", "cart-line-item");
+      let name = document.createElement("p");
+      name.textContent = item.name;
+      name.setAttribute("class", "cart-line-item");
 
-    let quantity = document.createElement("input");
-    quantity.setAttribute("type", "number");
-    quantity.setAttribute("value", item.amount);
-    quantity.setAttribute("class", "cart-line-item");
-    quantity.addEventListener("change", (event) => {
-      let newQuantity = parseInt(event.target.value);
-      if (newQuantity >= 0) {
-        cartArray[index].amount = newQuantity;
-        cartListMaker();
-      }
+      let quantity = document.createElement("input");
+      quantity.setAttribute("type", "number");
+      quantity.setAttribute("value", item.amount);
+      quantity.setAttribute("class", "cart-line-item");
+      quantity.addEventListener("change", (event) => {
+        let newQuantity = parseInt(event.target.value);
+        if (newQuantity >= 0) {
+          cartArray[index].amount = newQuantity;
+          cartListMaker();
+        }
+      });
+      let totalAmount = item.price * item.amount;
+      total += totalAmount;
+      let totalAmmountP = document.createElement("p");
+      totalAmmountP.setAttribute("class", "cart-line-item");
+      totalAmmountP.textContent = "$" + totalAmount.toFixed(2);
+      line.append(img, name, itemPrice, quantity, totalAmmountP);
+      cartList.appendChild(line);
     });
-    let totalAmount = item.price * item.amount;
-    total += totalAmount;
-    let totalAmmountP = document.createElement("p");
-    totalAmmountP.setAttribute("class", "cart-line-item");
-    totalAmmountP.textContent = "$" + totalAmount.toFixed(2);
-    line.append(img, name, itemPrice, quantity, totalAmmountP);
-    cartList.appendChild(line);
-  });
-  document.querySelector("#cart-container").appendChild(cartList);
-  let totalP = document.createElement("p");
-  totalP.textContent = "Total: $" + total.toFixed(2);
-  totalP.setAttribute("class", "cart-total");
-  document.querySelector("#cart-container").appendChild(totalP);
-}else{
-  let line = document.createElement("li");
+    document.querySelector("#cart-container").appendChild(cartList);
+    let totalholder = document.createElement("div");
+    totalholder.setAttribute("class", "total-holder");
+    let totalP = document.createElement("p");
+    totalP.textContent = "Total: $" + total.toFixed(2);
+    totalP.setAttribute("class", "cart-total");
+    totalholder.appendChild(totalP);
+    document.querySelector("#cart-container").appendChild(totalholder);
+  } else {
+    let line = document.createElement("li");
     line.setAttribute("class", "cart-line");
     line.textContent = "Cart is Empty";
     document.querySelector("#cart-container").appendChild(line);
+    let totalholder = document.createElement("div");
+    totalholder.setAttribute("class", "total-holder");
     let totalP = document.createElement("p");
-  totalP.textContent = "Total: $" + total.toFixed(2);
-  totalP.setAttribute("class", "cart-total");
-  document.querySelector("#cart-container").appendChild(totalP);
-}
+    totalP.textContent = "Total: $" + total.toFixed(2);
+    totalP.setAttribute("class", "cart-total");
+    totalholder.appendChild(totalP);
+    document.querySelector("#cart-container").appendChild(totalholder);
+  }
 }
 
 //boiler plate stuff
@@ -230,61 +238,58 @@ function clearPage() {
 }
 let cartContainer = document.querySelector("#cart-container");
 let cartBtn = document.querySelector("#cartBtn");
-cartBtn.addEventListener("click",() => {
+cartBtn.addEventListener("click", () => {
   if (cartContainer.style.display === "none") {
     cartContainer.style.display = "block";
   } else {
     cartContainer.style.display = "none";
   }
-  
-})
+});
 
-
-function addBanner(imgUrl){
+function addBanner(imgUrl) {
   topBanner = document.createElement("div");
   let bannerImg = document.createElement("img");
   bannerImg.src = imgUrl;
-  bannerImg.setAttribute("id", "logo");
-  bannerImg.setAttribute(
-    "style",
-    "height: 31.8vh; width: 111vh; padding-left: 0px;"
-  );
+  bannerImg.setAttribute("class", "banner");
+  
   bannerImg.setAttribute("alt", "logo");
   topBanner.append(bannerImg);
   document.querySelector("#Meat-Menu").appendChild(topBanner);
 }
 
-
-function renderCard(item){
+function renderCard(item) {
   let itemCard = document.createElement("div");
   itemCard.setAttribute("class", "card");
+
   let cardFront = document.createElement("div");
   cardFront.setAttribute("class", "card-front");
   cardFront.style.backgroundImage = `url(${item.image})`;
+
   let itemName = document.createElement("h3");
   itemName.setAttribute("class", "card-name");
   itemName.textContent = item.name;
+  
   cardFront.appendChild(itemName);
-  if(!item.inCart){
-  let favoriteBtnFront = document.createElement("button");
-  if (item.favorite) {
-    favoriteBtnFront.textContent = "★";
-  } else {
-    favoriteBtnFront.textContent = "☆";
-  }
-  favoriteBtnFront.setAttribute("class", "card-btn");
-  favoriteBtnFront.addEventListener("click", (event) => {
+  if (!item.inCart) {
+    let favoriteBtnFront = document.createElement("button");
     if (item.favorite) {
-      item.favorite = false;
-      favoriteBtnFront.textContent = "☆";
-    } else {
-      item.favorite = true;
       favoriteBtnFront.textContent = "★";
+    } else {
+      favoriteBtnFront.textContent = "☆";
     }
-    event.stopPropagation(); // prevent event from propagating to card
-  });
-  cardFront.appendChild(favoriteBtnFront);
-}
+    favoriteBtnFront.setAttribute("class", "card-btn");
+    favoriteBtnFront.addEventListener("click", (event) => {
+      if (item.favorite) {
+        item.favorite = false;
+        favoriteBtnFront.textContent = "☆";
+      } else {
+        item.favorite = true;
+        favoriteBtnFront.textContent = "★";
+      }
+      event.stopPropagation(); // prevent event from propagating to card
+    });
+    cardFront.appendChild(favoriteBtnFront);
+  }
   itemCard.appendChild(cardFront);
   // Create back face of the card with item details and add-to-cart button
   let CardBack = document.createElement("div");
@@ -295,8 +300,8 @@ function renderCard(item){
   itemDetails.textContent = item.description;
   let itemPrice = document.createElement("p");
   itemPrice.textContent = `$${item.price} per pound`;
-  itemDetails.appendChild(itemPrice);
-  itemDetails.append(addToCartForm(item));
+  //itemDetails.appendChild(itemPrice);
+  itemDetails.append(itemPrice, addToCartForm(item));
   CardBack.appendChild(itemDetails);
   itemCard.appendChild(CardBack);
   // Add click event listener to flip the card on click
@@ -305,12 +310,13 @@ function renderCard(item){
   });
   document.querySelector("#Meat-Menu").appendChild(itemCard);
 }
-function addHoverEffect(item){
+
+function addHoverEffect(item) {
   item.addEventListener("mouseover", () => {
     item.style.color = "#f00";
-    console.log("hey im hovering here")
-  })
+    console.log("hey im hovering here");
+  });
   item.addEventListener("mouseout", () => {
     item.style.color = "black";
-  })
+  });
 }
